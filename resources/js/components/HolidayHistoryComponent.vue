4cb9dcb14d1dfd5088eecc7706ff74abf2794d1c
sh4d1t0 <shadito@live.com.mx>
2019-10-03 13:13:11 -0500

    Add templates examples for auth, add holidayhistory component vue

---
+++ resources/js/components/HolidayHistoryComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Holiday History by Users</div>
+
+                    <div class="card-body">
+                        La tabla. {{ info }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['info'],
+        mounted() {
+            console.log('Component holiday history was mounted.')
+            axios.get('http://localhost:8000/api/v1/holidays/188').then(response => (this.info = response))
+        }
+    }
+</script>
---
+++ resources/views/holidays/holidayhistory.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <holiday-component></holiday-component>
+@endsection
